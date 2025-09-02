2db74745e03e9290a3c8e88005d2f6c679031dfe
Kyle <kylegranthousel@gmail.com>
2025-09-01 20:17:19 -0400

    Make helper methods

--- app/models/song.rb
+++ app/models/song.rb
@@ -2,8 +2,14 @@ class Song < ApplicationRecord
   belongs_to :artist, optional: true
 
   def artist_name
+    artist&.name
   end
 
   def artist_name=(name)
+    self.artist = if name.present?
+                    Artist.find_or_create_by(name: name)
+                  else
+                    nil
+                  end
   end
 end
