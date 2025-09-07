42dace5be3873cb2e1fcc3f9f5bb8e7a8b0f386f
Nihar Patel <niharpatel52@gmail.com>
2025-09-07 10:25:57 -0400

    Complete Lab

--- app/models/song.rb
+++ app/models/song.rb
@@ -2,8 +2,10 @@ class Song < ApplicationRecord
   belongs_to :artist, optional: true
 
   def artist_name
+    artist&.name
   end
 
   def artist_name=(name)
+    self.artist = Artist.find_or_create_by(name: name)
   end
 end
